1d9064222ff150b42ef85b6416ca3a6afe27a42b
admin <li_y_gen@163.com>
2019-07-01 00:39:31 +0800

    我是小弟

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,11 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("HelloWorld,我是张三!");
-<<<<<<< HEAD
-		System.out.println("我是大神,听我的!");
-		System.out.println("我是大神,继续呼唤!");
-=======
 		System.out.println("我是欧小弟,我说了算!");
->>>>>>> 237c6697f31c6dde5506a68181458574502ccf5b
+
     }
 }
