411525b3ac14f7bd3a74436807bba8e1f635bbae
admin <li_y_gen@163.com>
2019-07-01 00:07:21 +0800

    我是大神

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		 System.out.println("HelloWorld,我是张三!");
+		System.out.println("HelloWorld,我是张三!");
+		System.out.println("我是大神,听我的!");
     }
 }
