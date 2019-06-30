def9cdc5925144688a15656b554d5ed5cd702ef1
admin <li_y_gen@163.com>
2019-07-01 00:06:26 +0800

    我是小弟,我说了算

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		 System.out.println("HelloWorld,我是张三!");
+		System.out.println("HelloWorld,我是张三!");
+		System.out.println("我是欧小弟,我说了算!");
     }
 }
