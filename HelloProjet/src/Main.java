7752a31352170c924198710d87ab14bff938beed
admin <li_y_gen@163.com>
2019-07-01 10:25:27 +0800

    我是小弟,你们听我的

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("HelloWorld!");
 		System.out.println("HelloWorld,我是张三!");
 		System.out.println("我是大神,听我的召唤!");
+		System.out.println("我是小弟,我很拽!");
+		
     }
 }
