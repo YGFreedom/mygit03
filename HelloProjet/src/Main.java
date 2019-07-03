b2d55ae998a98178b3a907914c3a433aea14d5d7
admin <li_y_gen@163.com>
2019-07-03 11:45:33 +0800

    第一次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("蔡徐坤!");
+        System.out.println("java之父詹姆斯高世林!");
     }
 }
